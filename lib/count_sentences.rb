036081be7ff934d5bdd25f4f9098be6086476113
Stephen Olson <stephenolson@gmail.com>
2018-10-23 22:17:47 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,21 +7,11 @@ class String
   end
 
   def question?
-    if 
-      self.end_with?("?")
-      true
-    else
-      false
-    end
+    self.end_with?("?")
   end
 
   def exclamation?
-    if 
       self.end_with?("!")
-      true
-    else
-      false
-    end
   end
 
   def count_sentences
